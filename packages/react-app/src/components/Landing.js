280ee51bec44ff1dadc26d9374776460b9626a03
Kai <donkaixote@gmail.com>
2021-06-30 21:56:55 -0400

    update repo link

--- packages/react-app/src/components/Landing.js
+++ packages/react-app/src/components/Landing.js
@@ -12,14 +12,15 @@ import {
     createIcon,
     Center,
     HStack,
+    Link
 } from "@chakra-ui/react";
 // react-router
-import { Link } from "react-router-dom";
+import { Link as reactLink } from "react-router-dom";
 import UnifiLetterLogo from "./UnifiLetterLogo";
 
 export default function CallToActionWithAnnotation() {
     return (
-            <Center w='100%' h='100%'>
+        <Center w='100%' h='100%'>
             <Container maxW={"8xl"}>
                 <Stack
                     as={Box}
@@ -27,7 +28,7 @@ export default function CallToActionWithAnnotation() {
                     spacing={{ base: 4, md: 8 }}
                     py={{ base: 20, md: 36 }}
                 >
-                    <Heading fontSize={{ base: "8xl", sm: "8xl", md: "8xl", lg: "8xl" }}>
+                    <Heading fontSize={{ base: "4xl", sm: "6xl", md: "8xl", lg: "8xl" }}>
                         {UnifiLetterLogo()}
                     </Heading>
                     <Heading
@@ -38,13 +39,17 @@ export default function CallToActionWithAnnotation() {
                         <Text color="cyan.800"> Uniswap V3 Liquidity Manager</Text>
                     </Heading>
                     <Text fontSize="xl">Description - Place holder</Text>
+
                     <Stack
                         direction={"column"}
                         align={"center"}
                         alignSelf={"center"}
                         position={"relative"}
                     >
-                        <Link to="/app">
+                    <Link fontSize="xl" color="teal.600" href="https://github.com/phiiil/unifi" isExternal>
+                        Repo
+                    </Link>
+                        <reactLink to="/app">
                             <Button
                                 colorScheme={"teal"}
                                 rounded="xl"
@@ -53,10 +58,11 @@ export default function CallToActionWithAnnotation() {
                             >
                                 Enter Vault
                             </Button>
-                        </Link>
+                        </reactLink>
+
                     </Stack>
                 </Stack>
             </Container>
-            </Center>
+        </Center>
     );
 }
